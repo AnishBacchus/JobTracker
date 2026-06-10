39d43b956eaa2312b0eef77a6a7fd20cacc956b7
AnishBacchus <anishbacchus@gmail.com>
2026-06-10 18:05:50 -0400

    Worked on main.py, using argparse to use the add and get functions in the terminal.

---
+++ job_tracker/main.py
@@ -0,0 +1,34 @@
+import argparse
+from job_tracker.database import init_db, add_application, get_applications
+
+def main():
+    init_db()
+
+    parser = argparse.ArgumentParser(description="Job Application Tracker")
+    subparsers = parser.add_subparsers(dest="command")
+
+
+    #Add Job to tracker
+    add_parser = subparsers.add_parser("add")
+    add_parser.add_argument("--role", required=True)
+    add_parser.add_argument("--company", required=True)
+    add_parser.add_argument("--application_date")
+    add_parser.add_argument("--status", default="Applied")
+    add_parser.add_argument("--note")
+    
+
+
+    #Get all jobs from tracker
+    list_parser = subparsers.add_parser("all")
+
+
+    args = parser.parse_args()
+
+    if args.command == "add":
+        add_application(args.role, args.company, args.application_date, args.status, args.note)
+    elif args.command == "all":
+        print(get_applications())
+
+
+if __name__ == "__main__":
+    main()
